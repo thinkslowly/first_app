0a16c1367e447e489d706f0323a3395202bd76cd
think slowly <root@zhangmatoMacBook-Pro.local>
2013-06-22 11:51:02 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '178034f36ddda2419754c8d5680d123ed9f663c9694fafde710c1de25cfa80de9e838831db0dff0aba38ea829719488719b7e4e91ccda3222476769375ee1621'
